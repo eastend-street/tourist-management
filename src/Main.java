2b7f9ee06e2d4715a650717518728f25c53c7834
eastend-street <ss_1322irf@icloud.com>
2019-08-08 14:02:28 -0700

    add jar file

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     private int sample;
 
     public static void main(String[] args) {
-        UserLogin loginPage = new UserLogin();
+        Display display = new Display();
+//        UserLogin loginPage = new UserLogin();
     }
 }
